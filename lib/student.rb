1f9effae059cc83360f9cf292442264bd21877b2
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-16 22:51:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,13 +8,14 @@ def initialize(name, grade, id=nil)
 end
 
 def self.create_table
-  sql = <<-SQL
-  create TABLE IF NOT EXISTS STUDENTS (
-    id INTEGER PRIMARY KEY,
-    name TEXT
-    grade TEXT
-  )
-  SQL DB[:conn].execute(sql)
+  sql =  <<-SQL 
+     CREATE TABLE IF NOT EXISTS songs (
+       id INTEGER PRIMARY KEY, 
+       name TEXT, 
+       album TEXT
+       )
+       SQL
+   DB[:conn].execute(sql) 
   end
 end 
 
