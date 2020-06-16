9b88ea4dc097cca9abad96881c6c9288f7676dc3
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-16 22:54:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,11 +23,11 @@ end
 
 def save
 sql = <<-SQL
-INSERT INTO students (name, grade) 
+INSERT INTO students (name, grade)
 VALUES (?, ?)
-    SQL
+SQL
 
-    DB[:conn].execute(sql, self.name, self.album)
+DB[:conn].execute(sql, self.name, self.grade)
 
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
 
